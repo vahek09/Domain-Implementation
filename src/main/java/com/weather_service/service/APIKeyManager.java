8ce2f5368145336458702a330a3a5da7e56dce70
vahek09 <vkarapetyan@griddynamics.com>
2024-10-04 18:40:13 +0400

    Use of .env file to avoid Hard Coding for ApiKey (Used Dotenv)

--- src/main/java/com/weather_service/service/APIKeyManager.java
+++ src/main/java/com/weather_service/service/APIKeyManager.java
@@ -3,9 +3,11 @@ package com.weather_service.service;
 import com.weather_service.service.core.UserService;
 import com.weather_service.domain.User;
 import com.weather_service.interfaces.APIKeyManagerInterface;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class APIKeyManager  implements APIKeyManagerInterface {
     private final UserService userService;
+    private static final String API_KEY_ENV = "OPENWEATHER_API_KEY";
 
     public APIKeyManager(UserService userService) {
         this.userService = userService;
@@ -13,11 +15,13 @@ public class APIKeyManager  implements APIKeyManagerInterface {
 
     @Override
     public void assignAPIKey(User user) {
+        Dotenv dotenv = Dotenv.load();
+        String sharedApiKey = dotenv.get(API_KEY_ENV);
+
         if (user == null) {
             throw new IllegalArgumentException("User cannot be null");
         }
-        String apiKey = "null";
-        user.setApiKey(apiKey);
+        user.setApiKey(sharedApiKey);
         userService.updateUser(user);
         System.out.println("Assigned API key to user: " + user.getUsername());
     }
